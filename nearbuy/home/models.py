25f4bd5897e1401a7c64dcfe24498d3d20f934a3
Chay <chaitanya.sunny7@gmail.com>
2020-08-04 04:25:16 -0400

    added store model to admin page

--- nearbuy/home/models.py
+++ nearbuy/home/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class store(models.Model):     
+class Store(models.Model):     
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=25) 
     is_open = models.BooleanField(default=False)

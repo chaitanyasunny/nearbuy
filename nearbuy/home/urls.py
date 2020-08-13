3d1dd9513fe1774caf850330fdcae27e4da20fb5
Chay <chaitanya.sunny7@gmail.com>
2020-08-13 19:45:07 -0400

    updated store detailview

--- nearbuy/home/urls.py
+++ nearbuy/home/urls.py
@@ -1,13 +1,16 @@
 from rest_framework import routers
 from .api import StoreViewSet
-from django.urls import path 
-from .views import StoreView 
+from django.urls import path
+from django.conf.urls import url
+from .views import StoreView,ProductView,StoreDetailView
 
 router = routers.DefaultRouter()
 router.register('storeapi', StoreViewSet, 'store_api')
 
 urlpatterns = [
     path('stores', StoreView.as_view(), name='allstores'), 
+    path('store/<int:pk>', StoreDetailView.as_view(), name='store-detail'),
+    #path('stores/<int:id>', ProductView.as_view(), name='allproducts'),
     
 ]
 urlpatterns += router.urls
--- nearbuy/home/views.py
+++ nearbuy/home/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-from django.views.generic.list import ListView 
-from .models import Store
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Store,Product
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 
 class StoreView(ListView):
     model = Store
-    #template_name = "templates/stores.html"
+    context_object_name = 'all_stores'
+    def get_queryset(self):
+        return Store.objects.all()
+
+class StoreDetailView(DetailView):
+    model = Store
+    template_name = "home/store_details.html"
+    context_object_name = 'store'
+    
+class ProductView(ListView):
+    model = Store
+    
+
